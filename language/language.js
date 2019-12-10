75bba594e326dd7b093456b9fbeb6bd0979729e9
axinouni <1013945950@qq.com>
2019-12-10 15:52:28 +0800

    This is a demo of language internationalization

---
+++ language/language.js
@@ -0,0 +1,14 @@
+﻿var Language = {
+    "EN": {
+        "music": "music",
+        "basketball": "basketball",
+        "picture": "picture",
+        "calligraphy": "calligraphy"
+    },
+    "CN": {
+        "music": "音乐",
+        "basketball": "篮球",
+        "picture": "作画",
+        "calligraphy": "书法"
+    }
+}
